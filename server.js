994a6b84d292ba9b161ff67f2b4ca44375bbe561
Lawrence Jones <git@lwjon.es>
2022-03-08 09:59:56 -0800

    Add html routes

--- server.js
+++ server.js
@@ -1,16 +1,15 @@
-const fs = require('fs');
 const PORT = process.env.PORT || 3001;
 const express = require('express');
 const app = express();
 const apiRoutes = require('./routes/apiRoutes');
-// TODO: Require html routes
+const htmlRoutes = require('./routes/htmlRoutes');
 
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use('/api', apiRoutes);
-// TODO: Use html routes
+app.use('/', htmlRoutes);
 
 app.listen(PORT, () => {
   console.log(`  Server listening on port ${PORT}`);
